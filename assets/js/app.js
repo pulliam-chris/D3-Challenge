69b03baf980d8cf6c9275c61d9325102778aaadd
Chris Pulliam <chrispulliam.jobs@gmail.com>
2021-04-13 14:18:49 -0700

    adding dual axis labels to scatter x and y

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
-const svgWidth = 960;
-const svgHeight = 500;
+const svgWidth = 800;
+const svgHeight = 600;
 
 const margin = {
   top: 20,
   right: 40,
-  bottom: 60,
+  bottom: 80,
   left: 100
 };
 
@@ -72,24 +72,37 @@ d3.csv("assets/data/data.csv").then(censusData => {
     .attr("class", "stateText")
     .text(d => d.abbr)
     
-    // Create axes labels
+    // Create dual axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 20)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Lacks Healthcare (%)");
+    
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left + 40)
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Lacks Healthcare (%)");
+      .text("Smokes (%)");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
       .text("In Poverty (%)");
 
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 50})`)
+      .attr("class", "axisText")
+      .text("Income");
+
     // Step 1: Initialize Tooltip
     const toolTip = d3.tip()
     .attr("class", "d3-tip")
-    .offset([-10, 20])
+    .offset([20, 100])
     .html(d => `<strong>${d.state}<br /> Lacks Healthcare: ${d.healthcare}(%)<br />In Poverty: ${d.poverty}(%)`);
 
     // Step 2: Create the tooltip in chartGroup.
